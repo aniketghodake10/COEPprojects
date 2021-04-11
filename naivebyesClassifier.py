f627abd9b6d88303d7ca66f45d8a7ff42550078a
Aniket Ghodake <38704659+aniketghodake10@users.noreply.github.com>
2021-04-11 23:17:48 +0530

    Fix for zero frequency problem and Probability is normalized now

--- naivebyesClassifier.py
+++ naivebyesClassifier.py
@@ -7,12 +7,18 @@ data  = {'Days':['Weekday','Weekday','Weekday','Holiday','Saturday','Weekday','H
                  'Saturday','Weekday','Weekday','Weekday','Weekday','Saturday','Weekday','Holiday','Weekday','Weekday'],
          'Season':['Spring','Winter','Winter','Winter','Summer','Autumn','Summer','Summer','Winter','Summer','Spring',
                    'Summer','Winter','Summer','Winter','Autumn','Autumn','Spring','Spring','Spring'],
-         'Fog':['Nonee','Nonee','Nonee','High','Normal','Normal','High','Normal','High','Nonee','High','high','Normal',
-                'high','Normal','High','Nonee','Normal','Normal','Normal'],
+         'Fog':['Nonee','Nonee','Nonee','High','Normal','Normal','High','Normal','High','Nonee','High','High','Normal',
+                'High','Normal','High','Nonee','Normal','Normal','Normal'],
          'Rain':['None','Slight','None','Slight','None','None','Slight','None','Heavy','Slight','Heavy','Slight',
                  'None','None','Heavy','Slight','Heavy','Slight','None','Heavy'],
          'Class':['OnTime','OnTime','OnTime','Late','OnTime','VeryLate','OnTime','OnTime','VeryLate','OnTime',
                   'Cancelled','OnTime','Late','OnTime','VeryLate','OnTime','OnTime','OnTime','OnTime','OnTime']}
+
+days = ['Weekday','Holiday','Saturday','Sunday']
+season = ['Spring','Winter','Autumn','Summer']
+fog = ['Nonee','High','Normal']
+rain = ['None','Slight','Heavy']
+attributes = [days,season,fog,rain]
 df = pd.DataFrame(data)
 # print(df)
 
@@ -43,10 +49,19 @@ dff = pd.DataFrame(dff_data)
 
 dff1_data = {'Features':list(dff['Features']),'OnTime':[],'Late':[],'VeryLate':[],'Cancelled':[]}
 
+
 for i in ClassAttributes:
     summ = dff[:4].sum()[i]
     for j in range(len(dff[i])):
-        dff1_data[i].append(flsr(dff[i][j] / summ))
+        if summ != 0:
+            if flsr(dff[i][j] / summ) != 0:
+                dff1_data[i].append(flsr(dff[i][j] / summ))
+            else:
+                for v in attributes:
+                    if dff['Features'][j] in v:
+                        dff1_data[i].append(flsr((dff[i][j] + 1 ) / (len(v) + summ)))
+        else:
+            dff1_data[i].append(0)
 
 dff1 = pd.DataFrame(dff1_data)
 
@@ -54,6 +69,7 @@ aa = dff[:4].sum()['OnTime']
 bb = dff[:4].sum()['Late']
 cc = dff[:4].sum()['VeryLate']
 dd = dff[:4].sum()['Cancelled']
+
 
 dff_temp = pd.DataFrame({'Features':['Total'],'OnTime':[flsr(aa/(aa+bb+cc+dd))],'Late':[flsr(bb/(aa+bb+cc+dd))],
                          'VeryLate':[flsr(cc/(aa+bb+cc+dd))],'Cancelled':[flsr(dd/(aa+bb+cc+dd))]})
@@ -70,6 +86,8 @@ for i in ClassAttributes:
                           dff2[i][list(dff['Features']).index(instancee[2])] * dff2[i][list(dff['Features']).index(instancee[3])] *
                           dff2[i].iloc[-1])
 
-print('Probability of classes ','\n','OnTime = ',probability['OnTime'],
-      '\n','Late = ',probability['Late'],'\n','VeryLate = ',probability['VeryLate'],'\n','Cancelled = ',
-      probability['Cancelled'],'\n','\n','CLASS LABEL OF GIVEN INSTANCE IS ===   ',max(probability,key=probability.get).upper())
+sumb = sum(probability.values())
+
+print('Normalized Probability of classes ','\n','OnTime = ',flsr(probability['OnTime']/sumb),
+      '\n','Late = ',flsr(probability['Late']/sumb),'\n','VeryLate = ',flsr(probability['VeryLate']/sumb),'\n','Cancelled = ',
+      flsr(probability['Cancelled']/sumb),'\n','\n','CLASS LABEL OF GIVEN INSTANCE IS ===   ',max(probability,key=probability.get).upper())
